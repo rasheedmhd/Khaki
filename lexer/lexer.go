dd82252f4cae5e2227bd80acc6eb77bacce7da61
rasheedmhd <starletgh@gmail.com>
2024-12-06 23:29:37 +0000

    Add readChar function

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,12 +2,23 @@ package lexer
 
 type Lexer struct {
 	input        string
-	position     int // current char 
-	readPosition int // next char, used mostly for peeking 
+	position     int // current char
+	readPosition int // next char, used mostly for peeking
 	char         byte
 }
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.char = 0
+	} else {
+		l.char = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
 }
