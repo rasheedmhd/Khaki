e7bfa557295cc0eff5627e8aaa328da70ee5c1ba
rasheedmhd <starletgh@gmail.com>
2024-12-06 23:51:28 +0000

    Add NextToken and newToken functions

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "khaki/token"
+
 type Lexer struct {
 	input        string
 	position     int // current char
@@ -21,4 +23,37 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.char {
+	case '=':
+		tok = newToken(token.ASSIGN, l.char)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.char)
+	case '(':
+		tok = newToken(token.LPAREN, l.char)
+	case ')':
+		tok = newToken(token.RPAREN, l.char)
+	case ',':
+		tok = newToken(token.COMMA, l.char)
+	case '+':
+		tok = newToken(token.PLUS, l.char)
+	case '{':
+		tok = newToken(token.LBRACE, l.char)
+	case '}':
+		tok = newToken(token.RBRACE, l.char)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, char byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(char)}
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -28,7 +28,7 @@ func TestNexToken(t *testing.T) {
 	for i, tt := range tests {
 		tok := l.NextToken()
 
-		if tok.TokenType != tt.expectedType {
+		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
 		}
