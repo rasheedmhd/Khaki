58a8ebf41d7860c00d0aeb33e105e925edddb101
rasheedmhd <starletgh@gmail.com>
2024-12-08 22:43:11 +0000

    Rearrange funcitons

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,12 +9,6 @@ type Lexer struct {
 	char         byte
 }
 
-func New(input string) *Lexer {
-	l := &Lexer{input: input}
-	l.readChar()
-	return l
-}
-
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.char = 0
@@ -23,6 +17,28 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func newToken(tokenType token.TokenType, char byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(char)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.char) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(char byte) bool {
+	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
 }
 
 func (l *Lexer) NextToken() token.Token {
@@ -48,7 +64,7 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
-	default: 
+	default:
 		if isLetter(l.char) {
 			tok.Literal = l.readIdentifier()
 			return tok
@@ -60,19 +76,3 @@ func (l *Lexer) NextToken() token.Token {
 	l.readChar()
 	return tok
 }
-
-func newToken(tokenType token.TokenType, char byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(char)}
-}
-
-func (l *Lexer) readIdentifier() string {
-	position := l.position
-	for isLetter(l.char) {
-		l.readChar()
-	}
-	return l.input[position:l.position]
-}
-
-func isLetter(char byte) bool {
-	return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_' 
-} 
